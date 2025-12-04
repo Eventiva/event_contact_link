e863288f626ecb9792a692771d5fa7c23351e269
Jonathan Stevens <jonathan@resnovas.com>
2025-12-04 18:03:35 +0000

    Update version from 18.0.1.0.0 to 1.2.0 for Odoo 19 upgrade

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Event Contact Link',
-    'version': '18.0.1.0.0',
+    'version': '1.2.0',
     'category': 'Events',
     'summary': 'Link event registrations to contacts with automatic contact creation',
     'description': """
